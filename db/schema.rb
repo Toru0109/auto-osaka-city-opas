5c0e7f21a2fd90a53298f17539f8e7e276b0e8ae
吉川徹 <toruyoshikawa@yoshikawatr.local>
2022-04-09 14:57:33 +0900

    not to use foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,12 @@ ActiveRecord::Schema.define(version: 2022_03_19_013842) do
   enable_extension "plpgsql"
 
   create_table "automation_settings", force: :cascade do |t|
-    t.bigint "user_id"
+    t.integer "user_id"
     t.integer "sports_type", default: 0, null: false
     t.string "facility_types", limit: 256, default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", limit: 30, default: "", null: false
-    t.index ["user_id"], name: "index_automation_settings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,5 +32,4 @@ ActiveRecord::Schema.define(version: 2022_03_19_013842) do
     t.string "user_name", limit: 15, default: "", null: false
   end
 
-  add_foreign_key "automation_settings", "users"
 end
