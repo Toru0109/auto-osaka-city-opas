67165fcade21cb0242b8695a61e3a02a1843d96a
吉川徹 <toruyoshikawa@yoshikawatr.local>
2022-04-09 13:27:01 +0900

    change type into integer

--- db/migrate/20211221143540_create_automation_settings.rb
+++ db/migrate/20211221143540_create_automation_settings.rb
@@ -1,7 +1,7 @@
 class CreateAutomationSettings < ActiveRecord::Migration[5.2]
   def change
     create_table :automation_settings do |t|
-      t.references :user, foreign_key: true, type: :int
+      t.references :user, foreign_key: true, type: :integer
       t.integer :sports_type, null: false, default: 0
       t.string :facility_types, limit: 256, null: false, default: ''
 
