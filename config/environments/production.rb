b8caa87e09d04573fae64a0941cf8b329653ed29
吉川徹 <toruyoshikawa@yoshikawatr.local>
2022-04-16 10:36:12 +0900

    change production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # `public/` 以下のファイルをwebサーバーに提供するかの設定
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
