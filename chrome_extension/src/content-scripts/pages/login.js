fac42791f6de041996afb460fe8a14df17aba612
吉川徹 <toruyoshikawa@yoshikawatr.local>
2022-10-21 16:37:41 +0900

    change not to decrypt user number and password on chrome extension

--- chrome_extension/src/content-scripts/pages/login.js
+++ chrome_extension/src/content-scripts/pages/login.js
@@ -1,25 +1,12 @@
-import CryptoJS from "crypto-js";
-
 import getCurrentPathName from "../lib/get-current-pathname";
-import { userNumberKey, passwordKey } from "../lib/constans";
 
 export default () => {
   if (getCurrentPathName() != "/osakashi/Welcome.cgi") { return; }
 
   const params = new URL(window.location.href).searchParams;
-  const userNumber = getDecryptedData(params.get("n"), userNumberKey);
-  const password = getDecryptedData(params.get("p"), passwordKey);
 
-  document.getElementById("txtRiyoshaCode").value = userNumber
-  document.getElementById("txtPassWord").value = password
+  document.getElementById("txtRiyoshaCode").value = params.get("n");
+  document.getElementById("txtPassWord").value = params.get("p");
 
   setInterval(() => document.querySelector(".loginbtn > a").click(), 2000)
-}
-
-const getDecryptedData = (data, key) => {
-  let decryptedData;
-  for (let i = 0; i <= 2; i++) {
-    decryptedData = CryptoJS.AES.decrypt(data, key).toString(CryptoJS.enc.Utf8);
-    if (decryptedData) { return decryptedData; }
-  }
 }
